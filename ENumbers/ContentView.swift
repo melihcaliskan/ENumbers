2b6e2b28cafb096359c9817078985f2d0b7aa8f9
Melih Çalışkan <melih.caliskan@tapu.com>
2021-07-27 20:58:15 +0300

    first commit

---
+++ ENumbers/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ENumbers
+//
+//  Created by Melih Çalışkan on 26.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var rememberMe = false
+    
+    var body: some View {
+        VStack {
+            Text("e_numbers")
+        }
+        
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ENumbers/ENumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ENumbersApp.swift
+//  ENumbers
+//
+//  Created by Melih Çalışkan on 26.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct ENumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
